31d5b3325eab8c5ab95fa8a8b0405cb6ed4ae8d5
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-05 11:37:44 +0100

    remmapings

--- src/Foo.sol
+++ src/Foo.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-contract Foo {
-    function id(uint256 value) external pure returns (uint256) {
-        return value;
-    }
+import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract Token {
+    
+
 }

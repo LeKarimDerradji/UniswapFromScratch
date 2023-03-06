87cbc4339ddc10c5d5ec63c437c31d08d0adf29c
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-06 13:04:43 +0100

    custom_err

--- src/AMM.sol
+++ src/AMM.sol
@@ -6,13 +6,19 @@ import "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 contract AMM {
     address public tokenAddress;
 
+    error AddressZero();
+
     constructor(address tokenAddress_) {
         // check is contract
         // check is ERC20
-        require(tokenAddress_ != address(0), "token can not be address zero");
+        if (tokenAddress_ != address(0)) revert AddressZero();
 
         tokenAddress = tokenAddress_;
     }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          SETTERS                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /**
      * @dev this function serves for adding liquidity to the pool either in ether or in tokenAddress
@@ -21,5 +27,14 @@ contract AMM {
     function addLiquidity(uint256 tokenAmount_) external payable {
         IERC20 token = IERC20(tokenAddress);
         token.transferFrom(msg.sender, address(this), tokenAmount_);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                          GETTERS                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function getReserves() external view returns (uint256 tokenBalance_) {
+        uint256 tokenBalance = IERC20(tokenAddress).balanceOf(address(this));
+        return tokenBalance;
     }
 }

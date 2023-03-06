20358f326e2731dd79f249d5b68ea029eb0b007a
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-06 12:47:34 +0100

    addLiquidity

--- src/AMM.sol
+++ src/AMM.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
+import "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 contract AMM {
     address public tokenAddress;
 
@@ -10,5 +12,10 @@ contract AMM {
         require(tokenAddress_ != address(0), "token can not be address zero");
 
         tokenAddress = tokenAddress_;
+    }
+
+    function addLiquidity(uint256 tokenAmount_) external payable {
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), tokenAmount_);
     }
 }

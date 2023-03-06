a2fd3aa509ccb8a966f541363f69688c281e7256
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-03-06 12:49:27 +0100

    natspec

--- src/AMM.sol
+++ src/AMM.sol
@@ -14,6 +14,10 @@ contract AMM {
         tokenAddress = tokenAddress_;
     }
 
+    /**
+     * @dev this function serves for adding liquidity to the pool either in ether or in tokenAddress
+     * @param tokenAmount_ the amount of token to add to the liquidity pool (contract)
+     */
     function addLiquidity(uint256 tokenAmount_) external payable {
         IERC20 token = IERC20(tokenAddress);
         token.transferFrom(msg.sender, address(this), tokenAmount_);
